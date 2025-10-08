3a54b0e789f8519d1caf23bc2a51567f94c2ebe1
kormonditamas2222 <t.kormond2006@gmail.com>
2025-10-08 08:48:38 +0200

    dolgozók megjelennek, kiválaszthatók

--- dolgozok/MainWindow.xaml.cs
+++ dolgozok/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,10 +17,44 @@ namespace dolgozok
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        List<Dolgozo> dolgozok = [];
         public MainWindow()
         {
             InitializeComponent();
+            Beolvasas();
+            ListBoxFeltoltes();
+        }
+        private void Beolvasas()
+        {
+            StreamReader sr = new("dolgozok.txt");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string[] reszek = sr.ReadLine().Trim().Split(";");
+                Dolgozo dolgozo = new(reszek[0], reszek[1], reszek[2], reszek[3], int.Parse(reszek[4]), reszek[5]);
+                dolgozok.Add(dolgozo);
+            }
+        }
+        private void ListBoxFeltoltes()
+        {
+            List<string> nevek = [];
+            foreach (Dolgozo dolgozo in dolgozok)
+            {
+                nevek.Add(dolgozo.Nev);
+            }
+            lb_dolgozok.ItemsSource = nevek;
+        }
+
+        private void lb_dolgozok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string kivalasztott = lb_dolgozok.SelectedItem.ToString() ?? "";
+            foreach (Dolgozo dolgozo in dolgozok)
+            {
+                if (kivalasztott == dolgozo.Nev)
+                {
+                    lbl_dolgozonev.Content = dolgozo.ToString();
+                }
+            }
         }
     }
 }
