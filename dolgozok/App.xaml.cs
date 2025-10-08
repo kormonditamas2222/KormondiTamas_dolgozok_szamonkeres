2873a383bb19411562589e30a18515419f31958c
kormonditamas2222 <t.kormond2006@gmail.com>
2025-10-08 08:15:07 +0200

    Add project files.

---
+++ dolgozok/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace dolgozok
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
